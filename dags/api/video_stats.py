786974a7dc813f8dde32276a630f7bce34848af5
Wiktor Bielski <witeq996@gmail.com>
2026-05-15 14:04:40 +0200

    dags updates

--- dags/api/video_stats.py
+++ dags/api/video_stats.py
@@ -2,15 +2,20 @@ import requests
 import json
 from datetime import date
 
-import os
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
+# load_dotenv(dotenv_path="./.env")
 
-load_dotenv(dotenv_path="./.env")
+from airflow.decorators import task
+from airflow.models import Variable
 
-API_KEY = os.getenv("API_KEY")
+
+API_KEY = Variable.get("API_KEY")
 CHANNEL_HANDLE = "MrBeast"
 maxResults = 50
 
+
+@task
 def get_playlist_id():
 
     try:
@@ -37,7 +42,7 @@ def get_playlist_id():
         raise e
     
 
-
+@task
 def get_video_ids(playlistId):
 
     video_ids = []
@@ -76,6 +81,7 @@ def get_video_ids(playlistId):
         raise e
     
 
+@task
 def extract_video_data(video_ids):
 
     extracted_data = []
@@ -119,6 +125,8 @@ def extract_video_data(video_ids):
     except requests.exceptions.RequestException as e:
         raise e
     
+
+@task
 def save_to_json(extracted_data):
     file_path = f"./data/YT_data_{date.today()}.json"
 
